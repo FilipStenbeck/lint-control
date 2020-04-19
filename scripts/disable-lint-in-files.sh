8bece22c6e0512b463f83760f61d9c1752b6586f
Filip <filip.stenbeck@detectify.com>
2020-04-19 10:07:53 +0200

    add main

--- scripts/disable-lint-in-files.sh
+++ scripts/disable-lint-in-files.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 path_to_self="$(dirname "$0")"
+PATH_TO_FOLDER=$1
 
-find $PWD -name '*.js' -type f -exec $path_to_self/add-line-to-file.sh $1 {} \;
+find $PATH_TO_FOLDER -name '*.js' -type f -exec $path_to_self/add-line-to-file.sh {} \;
--- scripts/enable-lint-in-files.sh
+++ scripts/enable-lint-in-files.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+
+PATH_TO_FOLDER=$1
 path_to_self="$(dirname "$0")"
-find $PWD -name '*.js' -type f -exec $path_to_self/remove-line-in-file.sh $1 {} \;
+find $PATH_TO_FOLDER -name '*.js' -type f -exec $path_to_self/remove-line-in-file.sh {} \;
